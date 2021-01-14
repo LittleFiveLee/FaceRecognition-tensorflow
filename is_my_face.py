c85c529aef46348757eb12b1fb26b9bb14a3ec85
LittleFiveLee <2535904526@qq.com>
2021-01-14 17:40:02 +0800

    dlib + tensorflow + sklearn 人脸 识别

--- is_my_face.py
+++ is_my_face.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import cv2
 import dlib
 import numpy as np
@@ -127,36 +129,36 @@ def cnnLayer():
     out = tf.add(tf.matmul(dropf, Wout), bout)
     return out
 
-output = cnnLayer()  
-predict = tf.argmax(output, 1)  
-   
-saver = tf.train.Saver()  
-sess = tf.Session()  
-saver.restore(sess, tf.train.latest_checkpoint('.'))  
-   
-def is_my_face(image):  
-    res = sess.run(predict, feed_dict={x: [image/255.0], keep_prob_5:1.0, keep_prob_75: 1.0})  
-    if res[0] == 1:  
-        return True  
-    else:  
-        return False  
+output = cnnLayer()
+predict = tf.argmax(output, 1)
+
+saver = tf.train.Saver()
+sess = tf.Session()
+saver.restore(sess, tf.train.latest_checkpoint("/"))
+
+def is_my_face(image):
+    res = sess.run(predict, feed_dict={x: [image/255.0], keep_prob_5:1.0, keep_prob_75: 1.0})
+    if res[0] == 1:
+        return True
+    else:
+        return False
 
 #使用dlib自带的frontal_face_detector作为我们的特征提取器
 detector = dlib.get_frontal_face_detector()
 
-cam = cv2.VideoCapture(0)  
-   
-while True:  
-    _, img = cam.read()  
+cam = cv2.VideoCapture(0)
+
+while True:
+    _, img = cam.read()
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     dets = detector(gray_image, 1)
     if not len(dets):
         #print('Can`t get face.')
         cv2.imshow('img', img)
-        key = cv2.waitKey(30) & 0xff  
+        key = cv2.waitKey(30) & 0xff
         if key == 27:
             sys.exit(0)
-            
+
     for i, d in enumerate(dets):
         x1 = d.top() if d.top() > 0 else 0
         y1 = d.bottom() if d.bottom() > 0 else 0
@@ -172,5 +174,5 @@ while True:
         key = cv2.waitKey(30) & 0xff
         if key == 27:
             sys.exit(0)
-  
-sess.close() 
+
+sess.close()
